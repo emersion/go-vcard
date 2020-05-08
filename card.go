emersion/go-vcard

commit 8801711c93555561451857b712b3c2f496e9a987

fix: Preferred() returns field with highest PREF
According to https://tools.ietf.org/html/rfc6350#section-5.3:
`Its value MUST be an integer between 1 and 100 that
   quantifies the level of preference.  Lower values correspond to a
   higher level of preference, with 1 being most preferred.`

card.go
@@ -126,7 +126,7 @@ func (c Card) Preferred(k string) *Field {
 	}
 
 	field := fields[0]
-	max := 0
+	min := 100
 	for _, f := range fields {
 		n := 0
 		if pref := f.Params.Get(ParamPreferred); pref != "" {
@@ -136,8 +136,8 @@ func (c Card) Preferred(k string) *Field {
 			n = 1
 		}
 
-		if n > max {
-			max = n
+		if n < min {
+			min = n
 			field = f
 		}
 	}

commit dd3110a24ec26a6b9f8a45da21cc0f2ca2ce8c67

Fix Card.Preferred tests
Fixes: 8801711c9355 ("fix: Preferred() returns field with highest PREF")

card.go
@@ -128,7 +128,7 @@ func (c Card) Preferred(k string) *Field {
 	field := fields[0]
 	min := 100
 	for _, f := range fields {
-		n := 0
+		n := 100
 		if pref := f.Params.Get(ParamPreferred); pref != "" {
 			n, _ = strconv.Atoi(pref)
 		} else if f.Params.HasType("pref") {

card_test.go
@@ -187,8 +187,8 @@ func TestCard_Preferred(t *testing.T) {
 		},
 		{
 			"EMAIL": []*Field{
-				{Value: "me@example.org", Params: Params{"TYPE": {"home"}, "PREF": {"25"}}},
-				{Value: "me@example.com", Params: Params{"TYPE": {"work"}, "PREF": {"50"}}},
+				{Value: "me@example.org", Params: Params{"TYPE": {"home"}, "PREF": {"50"}}},
+				{Value: "me@example.com", Params: Params{"TYPE": {"work"}, "PREF": {"25"}}},
 			},
 		},
 		// v3.0